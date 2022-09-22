8937f2dbc02d78388698f10be8467a77065bfa42
julianParrax <julianfranco.parra@gmail.com>
2022-09-22 16:28:24 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import indexRoutes from './routes/index.js'
+const app = express();
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url)); //to get absolute path
+
+app.listen(3000);
+
+app.set("views", join(__dirname, "views"));
+//to say where the VIEWS folder is located
+app.set("view engine", "ejs");
+
+app.use(indexRoutes)
+
+app.use(express.static(join(__dirname,'public')))
+
+console.log("server is listening on port 3000");
+
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+//this folder will contain all my routes for a better organization
+
+import { Router } from "express";
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.render("index", {title: 'First website with node'});
+}); //res.render for work with the index.ejs
+
+router.get("/about", (req, res) => {
+  res.render("about", { title: 'About me'});
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact", {title: 'Contact page'});
+});
+
+export default router;
